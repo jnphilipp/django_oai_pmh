569fa06d2ff1ad164541966a86f1d7a3f17f2645
jnphilipp <mail@jnphilipp.org>
2018-02-18 22:20:56 +0100

    Add MetadataFormat model.

---
+++ models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of django_oai_pmh.
+#
+# django_oai_pmh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django_oai_pmh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django_oai_pmh. If not, see <http://www.gnu.org/licenses/>.
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .fields import SingleLineTextField
+
+
+class MetadataFormat(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    prefix = SingleLineTextField(unique=True)
+    schema = models.URLField(max_length=2048)
+    namespace = models.URLField(max_length=2048)
+
+    def __str__(self):
+        return self.prefix
+
+    class Meta:
+        ordering = ('prefix',)
+        verbose_name = _('Metadata format')
+        verbose_name_plural = _('Metadata formats')
