f785bbd3f9bb41abe536b13158713afc803eb80c
jnphilipp <mail@jnphilipp.org>
2018-02-19 20:22:52 +0100

    Add settings.

---
+++ settings.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of django_oai_pmh.
+#
+# django_oai_pmh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django_oai_pmh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django_oai_pmh. If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils import timezone
+
+
+USER_SETTINGS = getattr(settings, "OAI_PMH", {})
+
+if 'REPOSITORY_NAME' in USER_SETTINGS:
+    REPOSITORY_NAME = USER_SETTINGS['REPOSITORY_NAME']
+else:
+    raise ImproperlyConfigured('No value for REPOSITORY_NAME.')
+
+NUM_PER_PAGE = 100
+if 'NUM_PER_PAGE' in USER_SETTINGS:
+    NUM_PER_PAGE = USER_SETTINGS['NUM_PER_PAGE']
