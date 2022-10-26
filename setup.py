83f102d2aed0e2ed69adae345e6a4509136d3bc0
J. Nathanael Philipp <nathanael@philipp.land>
2022-10-26 14:59:07 +0200

    Update setup.py and add pyproject.toml.

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Framework :: Django",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
